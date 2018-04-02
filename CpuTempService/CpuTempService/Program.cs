6cd24c266ef4e908a1c37ec4279dda425fb777be
Marco Achtziger <marco@marco80er.de>
2018-04-02 13:35:23 +0200

    Initial project for cpu temperature service

---
+++ CpuTempService/CpuTempService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CpuTempService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
