6f98cfb1e83e5d317db3e06467588752d92913cb
Marco Achtziger <marco@marco80er.de>
2018-04-02 15:14:37 +0200

    First stupid version to read in temperature of Raspi and send updates if changed

--- CpuTempService/Program.cs
+++ CpuTempService/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Net.Mime;
+using System.Threading;
 
 namespace CpuTempService
 {
@@ -6,7 +9,49 @@ namespace CpuTempService
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var notifier = new NotifyIfContentChanged("/sys/class/thermal/thermal_zone0/temp");
+            notifier.ContentChanged += NotifierOnContentChanged;
+            while (true)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Waiting....");
+            }
         }
+
+        private static void NotifierOnContentChanged(object sender, string s)
+        {
+            var temp = Int32.Parse(s);
+            var tempInFloat = (float) temp / 1000.0;
+            Console.WriteLine("Temperatur is : {0:0.00}", tempInFloat);
+        }
+    }
+
+    public class NotifyIfContentChanged
+    {
+        private String PreviousContent = string.Empty;
+
+        private FileInfo FileToWatch { get; set; }
+
+        private Timer Timer;
+
+        public event EventHandler<string> ContentChanged;  
+
+        private void Callback(object state)
+        {
+            var content = File.ReadAllText(FileToWatch.FullName);
+            if (PreviousContent != content)
+            {
+                PreviousContent = content;
+                ContentChanged(null, content);
+            }
+        }
+
+        public NotifyIfContentChanged(string path)
+        {
+            FileToWatch = new FileInfo(path);
+            Timer = new Timer(Callback, null, 0, 100);
+        }
+
+
     }
 }
