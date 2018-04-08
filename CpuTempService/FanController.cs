753ce10eeb5357aec64cbac1400f76a0bd0b1f7b
Marco Achtziger <marco@marco80er.de>
2018-04-08 12:03:39 +0200

    Added first version for Fan Controller service

---
+++ CpuTempService/FanController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Unosquare.RaspberryIO;
+using Unosquare.RaspberryIO.Gpio;
+
+namespace CpuTempService
+{
+    [Route(("fan"))]
+    public class FanController : Controller
+    {
+        private GpioPin OutputPin;
+
+        public FanController()
+        {
+            OutputPin = Pi.Gpio[0];
+            OutputPin.PinMode = GpioPinDriveMode.Output;
+        }
+        [HttpGet("on")]
+        public IActionResult On()
+        {
+            if (!OutputPin.Read())
+            {
+                OutputPin.Write(true);
+            }
+            return Ok();
+        }
+
+        [HttpGet("off")]
+        public IActionResult Off()
+        {
+            if (OutputPin.Read())
+            {
+                OutputPin.Write(false);
+            }
+            return Ok();
+        }
+    }
+}
