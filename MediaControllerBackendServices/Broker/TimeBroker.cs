8e3f508c18dc2fd67c3716b57e6f0c1a5fc7b718
Marco Achtziger <marco@marco80er.de>
2019-01-20 14:14:32 +0100

    Now also send time data through mqtt (because i can ;))

---
+++ MediaControllerBackendServices/Broker/TimeBroker.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using System.Timers;
+using Timer = System.Timers.Timer;
+using MediaControllerBackendServices.Messaging;
+using Newtonsoft.Json;
+
+namespace MediaControllerBackendServices.Broker
+{
+    internal class TimeBroker
+    {
+        private IMessageBus MessageBus { get; }
+        private Timer Timer { get; set; }
+        private static string myTopic = "time_data";
+        private static int myLastMinute;
+        public TimeBroker(IMessageBus messageBus)
+        {
+            MessageBus = messageBus;
+            myLastMinute = -1;
+            TimerElapsed(null, null);
+            myLastMinute = DateTime.Now.Minute;
+            StartTimerAfterFullMinute();
+        }
+
+        public void StartTimerAfterFullMinute()
+        {
+            var sleepSeconds = 59 - DateTime.Now.Second;
+            Thread.Sleep(sleepSeconds * 1000);
+            Timer = new Timer
+            {
+                Interval = 500
+            };
+            Timer.Elapsed += TimerElapsed;
+            Timer.Start();
+        }
+
+        private async void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            try
+            {
+                var time = new Time();
+                if (time.Minute != myLastMinute)
+                {
+                    myLastMinute = time.Minute;
+                    var payload = JsonConvert.SerializeObject(time);
+                    var message = new Message(myTopic, payload);
+                    MessageBus.SendMessage(message);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+
+        }
+
+        private class Time
+        {
+            public int Hour { get; }
+            public int Minute { get; }
+            public int Day { get; }
+            public int Month { get; }
+            public int Year { get; }
+            public string Dayname { get; }
+
+            public Time()
+            {
+                var now = DateTime.Now;
+                Hour = now.Hour;
+                Minute = now.Minute;
+                Day = now.Day;
+                Month = now.Month;
+                Year = now.Year;
+                Dayname = ResolveDayname(now.DayOfWeek);
+            }
+
+            public string ResolveDayname(DayOfWeek day)
+            {
+                switch (day)
+                {
+                    case DayOfWeek.Friday:
+                        return "Freitag";
+                    case DayOfWeek.Monday:
+                        return "Montag";
+                    case DayOfWeek.Saturday:
+                        return "Samstag";
+                    case DayOfWeek.Sunday:
+                        return "Sonntag";
+                    case DayOfWeek.Thursday:
+                        return "Donnerstag";
+                    case DayOfWeek.Tuesday:
+                        return "Dienstag";
+                    case DayOfWeek.Wednesday:
+                        return "Mittwoch";
+                    default:
+                        return "Don't Panik! 42!";
+                }
+            }
+
+        }
+    }
+
+}
--- MediaControllerBackendServices/Program.cs
+++ MediaControllerBackendServices/Program.cs
@@ -15,6 +15,7 @@ namespace MediaControllerBackendServices
             // start a MQTT backend e.g. with eclipse-mosquitto and "docker run -it -p 1883:1883 -p 9001:9001 eclipse-mosquitto"
             var bus = new MessageBus("RaspiBackend", "127.0.0.1", 1883);
             var broker = new WeatherBroker(bus);
+            var timer = new TimeBroker(bus);
             while (true)
             {
                 Thread.Sleep(1000);
