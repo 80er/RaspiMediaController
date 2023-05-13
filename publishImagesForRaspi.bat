72bf8c7e924387adc17a6fe4031a7a17223db9c0
Marco Achtziger <marco.achtziger@siemens-healthineers.com>
2023-05-13 23:42:41 +0200

    updated according to new deployments

--- publishImagesForRaspi.bat
+++ publishImagesForRaspi.bat
@@ -1,5 +1,5 @@
+cd C:\Users\Chef\source\repos\80er\RaspiMediaController\MediaControllerBackendServices
+docker build -t 80er/mediacontrollerbackendservice:LATEST --push .
 cd C:\Users\Chef\source\repos\80er\RaspiMediaController\RaspiFrontendAngular
 docker buildx create --name armv7builder --use
 docker buildx build --platform linux/arm/v7 -t 80er/netatmoweatherstationdisplay:LATEST --push .
-cd C:\Users\Chef\source\repos\80er\RaspiMediaController\MediaControllerBackendServices
-docker buildx build --platform linux/arm/v7 -t 80er/mediacontrollerbackendservice:LATEST --push .
